b67eb46bb297673e6d51fa0e24e33d851e1ba741
roshray <roshanroy759@gmail.com>
2024-09-05 01:24:10 +0530

    convex setup & mutation,query

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 "use client"
 
+import { api } from "@/convex/_generated/api";
 import { SignInButton, UserButton } from "@clerk/nextjs";
-import { Unauthenticated, Authenticated } from "convex/react";
+import { Unauthenticated, Authenticated, useMutation, useQuery } from "convex/react";
+import { title } from "process";
 
 
 export default function Home() {
+
+  const documents = useQuery(api.documents.getDocuments)
+  const createDocument = useMutation(api.documents.createDocument)
+  
   return (
     <main className="flex ">
       <Unauthenticated>
@@ -12,6 +18,11 @@ export default function Home() {
       </Unauthenticated>
       <Authenticated>
         <UserButton />
+        <button onClick={() => {createDocument({title: "hello world"})}} className="text-white">Click Me</button>
+
+        {documents?.map((doc) => (
+          <div key={doc.id}>{doc.title}</div>
+        ))}
       </Authenticated>
     </main>
 
---
+++ convex/documents.ts
@@ -0,0 +1,23 @@
+import { title } from "process";
+import { mutation, query } from "./_generated/server";
+import { v } from "convex/values";
+
+
+export const getDocuments =  query({
+    async handler(ctx) {
+        return await ctx.db.query('documents').collect()
+    },
+})
+
+
+export const createDocument = mutation({
+    args: {
+        title: v.string()
+    },
+    async handler(ctx, args) {
+
+        await ctx.db.insert('documents', {
+            title: args.title
+        })
+    }
+})
